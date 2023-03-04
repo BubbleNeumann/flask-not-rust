828f753e9d4e2b7ef59dc6ea67bd6bd6b7b2db7b
BubbleNeumann <bubbleneumann@gmail.com>
2023-03-04 15:48:57 +0400

    Fandom & Tag classes

--- app/models.py
+++ app/models.py
@@ -47,7 +47,14 @@ class User:
     def username_is_available(username: str) -> bool:
         return (
             len(db.run_select(query=f'select * from Users where username = {username}'))
-            > 0
+            == 0
+        )
+
+    @staticmethod
+    def email_is_available(email: str) -> bool:
+        return (
+            len(db.run_select(query=f'select * from Users where email = {email}'))
+            == 0
         )
 
     def __repr__(self) -> str:
@@ -81,6 +88,14 @@ class Text:
                 on Tags.id = Texts_Tags.tag_id where Texts_Tags.text_id = {self.id}'
         return db.run_select(query=query)
 
+    def add_tag(self, tag_id: int, con=None) -> None:
+        db.modify_table(query=f'insert into Texts_Tags (text_id, tag_id) \
+                values ("{self.id}", "{tag_id}")', con=con)
+
+    def add_fandom(self, fandom_id: int, con=None) -> None:
+        db.modify_table(query=f'insert into Texts_Fandoms (text_id, fandom_id) \
+                values ("{self.id}", "{fandom_id}")', con=con)
+
     def write_to_db(self, user_id: int, con=None) -> None:
         """
         Writes self object to db.
@@ -113,21 +128,53 @@ class Text:
 
 
 class Tag:
-    def __init__(self):
-        pass
+    def __init__(self, id, name) -> None:
+        self.id = id
+        self.name = name
 
     @staticmethod
-    def get_tag_by_id():
-        pass
+    def get_tag_by_id(id: int):
+        return Tag(*db.run_select(query=f'select * from Tags where id = {id}'))
 
     @staticmethod
-    def get_all_tags():
-        pass
+    def get_all_tags() -> list:
+        return db.run_select(query='select * from Tags')
 
-    def delete_from_db(self):
-        pass
+    # TODO test this
+    def write_to_db(self, con=None):
+        db.modify_table(query=f'insert into Tags (name) values ("{self.name}")', con=con)
 
-    def get_texts(self):
-        pass
+    def delete_from_db(self, con=None):
+        db.modify_table(query=f'delete from Tags where id = {id}', con=con)
+        db.modify_table(query=f'delete from Texts_Tags where tag_id = {id}', con=con)
+
+    def get_texts(self) -> list:
+        texts = db.run_select(query=f'select Texts.* from Texts join Texts_Tags \
+                on Texts.id = Texts_Tags.text_id where Texts_Tags.tag_id = {id}')
+        return [Text(*text) for text in texts]
 
 
+class Fandom:
+    def __init__(self, id, name) -> None:
+        self.id = id
+        self.name = name
+
+    @staticmethod
+    def get_all_fandoms() -> list:
+        return db.run_select(query='select * from Fandoms')
+
+    @staticmethod
+    def get_fandom_by_id(id: int):
+        return Fandom(*db.run_select(query=f'select * from Fandoms where id = {id}'))
+
+    def write_to_db(self, con=None) -> None:
+        self.id = db.modify_table(query=f'insert into Fandoms (name) values ("{self.name}")', con=con)
+
+    def delete_from_db(self, con=None):
+        db.modify_table(query=f'delete from Fandoms where id = {id}', con=con)
+        db.modify_table(query=f'delete from Texts_Fandoms where tag_id = {id}', con=con)
+
+    def get_texts(self, id: int) -> list:
+        texts = db.run_select(query=f'select Texts.* from Texts join Texts_Fandoms \
+                on Texts.id = Texts_Tags.text_id whre Texts_Fandoms.fandom_id = {id}')
+        return [Text(*text) for text in texts]
